eece9885015c1966f39c991f83306f9a84f3ab4f
Steven Lawson <steven.lawson@healthsqyre.com>
2024-02-19 12:46:45 -0700

    better second solution for anagrams with sort

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,40 +8,57 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// SOLUTION B with .sort CAN ALSO BE USED WITH CHARS, not just nums
+// WAAAAAY BETTER and easier solution
+
 function anagrams(stringA, stringB) {
-	const aCharMap = buildCharMap(stringA);
-	const bCharMap = buildCharMap(stringB);
-
-	// // EDGE CASE TO check if one obj has a char that the other does not.
-	// 	// => pull out all the keys and compare the two maps together
-	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-		return false;
-	}
-
-	// to iterate of an OBJECT use IN
-	for (let char in aCharMap) {
-		if (aCharMap[char] !== bCharMap[char]) {
-			// if NOT we know something is wrong -- do not line up at all
-			return false;
-		}
-	}
-
-	return true;
+	// 1. write helper remove spaces and punctuations
+	return cleanString(stringA) === cleanString(stringB)
 }
 
-function buildCharMap(str) {
-	// 	// helper function to make char maps
-
-	// 	// strip out any nonchars === regex
-	// 	// convert all to lower with toLowerCase
-	const charMap = {};
-
-	for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-		charMap[char] = charMap[char] + 1 || 1;
-	}
-
-	return charMap;
+function cleanString(str) {
+	return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
+
+
+
+
+// SOLUTION A
+
+// function anagrams(stringA, stringB) {
+// 	const aCharMap = buildCharMap(stringA);
+// 	const bCharMap = buildCharMap(stringB);
+
+// 	// // EDGE CASE TO check if one obj has a char that the other does not.
+// 	// 	// => pull out all the keys and compare the two maps together
+// 	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+// 		return false;
+// 	}
+
+// 	// to iterate of an OBJECT use IN
+// 	for (let char in aCharMap) {
+// 		if (aCharMap[char] !== bCharMap[char]) {
+// 			// if NOT we know something is wrong -- do not line up at all
+// 			return false;
+// 		}
+// 	}
+
+// 	return true;
+// }
+
+// function buildCharMap(str) {
+// 	// 	// helper function to make char maps
+
+// 	// 	// strip out any nonchars === regex
+// 	// 	// convert all to lower with toLowerCase
+// 	const charMap = {};
+
+// 	for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+// 		charMap[char] = charMap[char] + 1 || 1;
+// 	}
+
+// 	return charMap;
+// }
 
 
 module.exports = anagrams;
