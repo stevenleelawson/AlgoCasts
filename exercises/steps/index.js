037a4a0a36f4174373778944ef377d7bca9b5d21
Steven Lawson <steven.lawson@healthsqyre.com>
2024-02-21 13:13:52 -0700

    iterative solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,24 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+	// ITERATIVE SOLUTION NUMBER ONE
+	// this loop is for the 'row'
+	for (let row = 0; row < n; row++) {
+		// create a var to hold the #s
+		let stair = ''
+		// for loop for the 'column;
+		for (let col = 0; col < n; col++) {
+			// IF the current column is equal to or less than the current row, add a '# to stair
+			if (col <= row) {
+				stair += '#'
+			} else {
+				stair += ' '
+			}
+
+		}
+		console.log(stair)
+	}
+}
 
 module.exports = steps;
