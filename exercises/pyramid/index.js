3b965b1bdd73e48ccc3d9e7b09add947e10ea43e
Steven Lawson <steven.lawson@healthsqyre.com>
2024-02-22 14:07:38 -0700

    damn

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,32 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+	// midpoint calc
+	const mid = Math.floor((2 * n - 1) / 2);
+
+	// define BASE CASE
+	if (n === row) {
+		return
+	}
+
+	//increment row
+	if (level.length === 2 * n - 1) {
+		console.log(level)
+
+		// recursion bit and change arg
+		return pyramid(n, row + 1)
+	}
+
+	let add;
+
+	if (mid - row <= level.length && mid + row >= level.length) {
+		add = '#'
+	} else {
+		add = ' '
+	}
+
+	pyramid(n, row, level + add)
+}
 
 module.exports = pyramid;
